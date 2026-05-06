914e583b458794c58a5f5268da9700dbd8fba253
Jose <josejfadvincula@gmail.com>
2026-05-06 11:51:08 -0300

    actualización de archivos base
    definición de clases y de interfaces base

--- src/garage/Camion.java
+++ src/garage/Camion.java
@@ -1,3 +1,17 @@
-public class Camion {
+public class Camion extends Vehiculo {
 
+    public Camion(String patente, String marca, String modelo, int horasEstimadas) {
+        super(patente, marca, modelo, horasEstimadas);
+    }
+
+    public double calcularCosto() {
+        return 0;
+    }
+
+    public int obtenerEspacios() {
+        return 0;
+    }
+
+    public void mostrarDatos() {
+    }
 }
--- src/garage/Vehiculo.java
+++ src/garage/Vehiculo.java
@@ -1,3 +1,24 @@
-public class Vehiculo {
+public abstract class Vehiculo implements Calculable, Mostrable {
 
+    private String patente;
+    private String marca;
+    private String modelo;
+    private int horasEstimadas;
+
+    public Vehiculo(String patente, String marca, String modelo, int horasEstimadas) {
+        this.patente = patente;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.horasEstimadas = horasEstimadas;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public int getHorasEstimadas() {
+        return horasEstimadas;
+    }
+
+    public abstract int obtenerEspacios();
 }
